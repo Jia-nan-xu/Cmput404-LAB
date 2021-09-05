6a5b277a2ae992ee7b685b00c2502e07af37195e
hhong1 <44427485+ZacOrz@users.noreply.github.com>
2021-09-05 17:46:45 -0600

    Update q2.py

--- q2.py
+++ q2.py
@@ -9,5 +9,5 @@ print(r.text)                           #print the text of the website we get
 git_url = "https://raw.githubusercontent.com/ZacOrz/Cmput404/main/q2.py"  #this  is the raw url from github
 
 r_git = urllib.request.urlretrieve(git_url, "q2.py")    #download the  file from github
-git_txt  = requests.get(git_url)                        #geet the code and print it
+git_txt  = requests.get(git_url)                        #get the code and print it
 print(git_txt.text)
